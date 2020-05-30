689f92359e540d0f3cbd773522f911a5feace427
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2020-05-31 02:09:04 +0800

    Update ffn_no_norm.py

--- core/models/ffn_no_norm.py
+++ core/models/ffn_no_norm.py
@@ -22,7 +22,7 @@ class ResBlock(nn.Module):
         return conv1_out + x
 
 
-class FFN(nn.Module):
+class FFN_no_norm(nn.Module):
     def __init__(self, in_channels=2, mid_channels=32, out_channels=1, kernel_size=(3, 3, 3), padding=1, depth=12,
                  input_size=[33, 33, 33], delta=[8, 8, 8]):
         super(FFN, self).__init__()
