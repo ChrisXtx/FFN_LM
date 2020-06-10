a549395f146fe12f3c5e88f4ce0abc6618d64b3d
Shubhra Pandit <shubhra.pandit@gmail.com>
2020-06-09 21:34:52 -0400

    Read and process seed list
    - Read in the seed list
    - Call segment_at for each seed in this list

--- inference_run.py
+++ inference_run.py
@@ -8,17 +8,24 @@ import numpy as np
 from core.models.ffn import FFN
 from core.data.utils import Canvas
 
-parser = argparse.ArgumentParser(description='inference script')
-parser.add_argument('--data', type=str, default='', help='input images')
+parser = argparse.ArgumentParser(description='Inference script')
+parser.add_argument('--data', type=str, default='', help='Input images')
 parser.add_argument('--model', type=str, default='',
-                    help='path to ffn model')
-parser.add_argument('--delta', default=(4, 4, 4),
-                    help='delta offset')
-parser.add_argument('--input_size', default=(39, 39, 39), help='input size')
-parser.add_argument('--depth', type=int, default=12, help='depth of ffn')
-parser.add_argument('--seg_thr', type=float, default=0.6, help='input size')
-parser.add_argument('--mov_thr', type=float, default=0.7, help='input size')
-parser.add_argument('--act_thr', type=float, default=0.8, help='input size')
+                    help='Path to FFN model')
+parser.add_argument('--seeds', type=str, default=None,
+                    help='Path to seed coordinates file')
+parser.add_argument('--output', type=str, default='',
+                    help='Path to output files')
+parser.add_argument('--delta', default=(15, 15, 15),
+                    help='Delta offset')
+parser.add_argument('--input_size', default=(51, 51, 51), help='Input size')
+parser.add_argument('--depth', type=int, default=26, help='Depth of FFN')
+parser.add_argument('--seg_thr', type=float, default=0.6,
+                    help='Segment threshold')
+parser.add_argument('--mov_thr', type=float, default=0.7,
+                    help='Move threshold')
+parser.add_argument('--act_thr', type=float, default=0.8,
+                    help='Act threshold')
 args = parser.parse_args()
 
 
@@ -47,14 +54,29 @@ def run():
 
     model.eval()
 
+    # Open the input image file and read the stack of images
     with h5py.File(args.data, 'r') as f:
         images = (f['/image'][()].astype(np.float32) - 128) / 33
 
     seed_list = []
-    canva = Canvas(model, images, seed_list, args.input_size, args.delta,
-                   args.seg_thr, args.mov_thr, args.act_thr)
+    if args.seeds is not None:
+        # Open the seed coordinate file and read in the seeds
+        with h5py.File(args.seeds, 'r') as f:
+            seed_list = f['/inference_coor'][()].astype(np.int64)
 
-    canva.segment_at((106, 137, 173), 2)
+    canva = Canvas(model, images, seed_list, args.input_size, args.delta,
+                   args.seg_thr, args.mov_thr, args.act_thr, args.output)
+
+    # TODO: Modify segment_all to process seed_list
+    # For now use segment_at instead
+    if len(seed_list) > 0:
+        # Segment for each seed position
+        for seed in seed_list:
+            canva.segment_at((seed[0], seed[1], seed[2]), 0)
+    else:
+        # Manually specify a seed location
+        # TODO: input seed location from user
+        canva.segment_at((42, 184, 40), 0)
 
     # canva.segment_all()
 
