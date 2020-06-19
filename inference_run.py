c49c63f2982f7afaeebe5d1cb2aeb5592370c7bd
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2020-06-20 02:46:35 +0800

    Update inference_run.py

--- inference_run.py
+++ inference_run.py
@@ -4,17 +4,17 @@ import argparse
 from core.models.ffn import FFN
 from core.data.utils import *
 from core.tool.tools import *
-from core.tool.SWC.swc_parser import *
+
 
 parser = argparse.ArgumentParser(description='inference script')
 parser.add_argument('--data', type=str,
                     default='/home/x903102883/FFN_LM_v0.2/data/agglomeration_net_test/pf_axonal_1/pf_raw_his_axonal_1_dim_down2.tif',
                     help='input images')
-parser.add_argument('--swc', type=str,
-                    default='/home/x903102883/2017EXBB/PF_inf/pf/axnol/axnol_fused.swc',
+parser.add_argument('--seed', type=str,
+                    default='/home/x903102883/FFN_LM_v0.2/data/agglomeration_net_test/pf_axonal_1/seeds.h5',
                     help='swc_skeletons')
 parser.add_argument('--model', type=str,
-                    default='/home/x903102883/FFN_LM_v0.2/model/down_2_adamffn_model_fov:39_delta:4_depth:26_recall86.85200922666019.pth',
+                    default='/home/x903102883/FFN_LM_v0.2/model/down_2_adamffn_model_fov:39_delta:4_depth:26_recall84.20619687414313.pth',
                     help='path to ffn model')
 
 parser.add_argument('--data_save', type=str,
@@ -33,6 +33,8 @@ parser.add_argument('--act_thr', type=float, default=0.8, help='activation of se
 parser.add_argument('--re_seg_thr', type=int, default=3, help='will not seed here if segmented many times')
 parser.add_argument('--vox_thr', type=int, default=500, help='remove if too small')
 parser.add_argument('--resume_seed', type=int, default=0, help='resume_seed')
+parser.add_argument('--tag', type=str, default='', help='tag the files')
+
 
 args = parser.parse_args()
 
@@ -49,32 +51,28 @@ def run():
     else:
         images = ((skimage.io.imread(args.data)).astype(np.float32) - 128) / 33
 
-    inf_seed_display = np.zeros(images.shape[:-1])
-    inf_seed_dict, inf_seed_list = swc_to_inf_seeds(args.swc, images.shape, args.swc_scaling,
-                                                    args.input_size, args.delta, args.buffer_distance, inf_seed_display,
-                                                    args.data_save)
-
-    with h5py.File(args.data[:-4] + "inf_record.h5", 'w') as f:
-        f.create_dataset('image_raw', data=images, compression='gzip')
-        f.create_dataset('inf_coords', data=inf_seed_list, compression='gzip')
-        f.create_dataset('inf_seed_display', data=inf_seed_display, compression='gzip')
 
     canvas_inf = Canvas(model, images, args.input_size, args.delta, args.seg_thr, args.mov_thr,
-                        args.act_thr, args.re_seg_thr, args.vox_thr, args.data_save, args.save_chunk,args.resume_seed)
+                        args.act_thr, args.re_seg_thr, args.vox_thr, args.data_save, args.save_chunk, args.resume_seed)
+
+    print(images.shape)
+    inf_seed_dict = {}
+    with h5py.File(args.seed, 'r') as segs:
+        seeds = segs['seeds'][()]
+        seed_id = 1
+        for coord in seeds:
+            inf_seed_dict[seed_id] = coord
+            seed_id += 1
 
 
     # run inference on every seed and save their segmentation
-
-    resume_cnt = 0
-    inf_cnt = 0
     for seed_id in inf_seed_dict.keys():
-        inf_cnt += 1
-        if inf_cnt < resume_cnt:
+        if seed_id < args.resume_seed:
             continue
-
         coord = inf_seed_dict[seed_id]
+        coord = tuple(coord)
+        if canvas_inf.segment_at(coord, seed_id,args.tag):
 
-        if canvas_inf.segment_at(coord, seed_id):
             continue
 
 
