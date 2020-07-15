68e993d0fbbeaa806efa0baf939aa8c07c2262ca
Shubhra Pandit <shubhra.pandit@gmail.com>
2020-07-15 17:35:22 -0400

    Some refactoring of inference_run.py and tools.py
    -Separated out load model functionality into its own method
    -Separated out load seeds file functionality into its own method
    -Added support for loading manual seeds text file
    -Improved docstrings, removed unused imports & code, added type hints
    -Added changes as per review to use f string in print statements

--- inference_run.py
+++ inference_run.py
@@ -1,26 +1,26 @@
-"""
-Runs inference.
+"""Runs inference on both manually provided and automatically generated seeds.
+
+Inference is multi-processed.
 """
 import os
 import argparse
-from typing import Dict, Tuple
+from typing import Dict
 from core.models.ffn import FFN
 import core.tool.tools as tools
 import threading
 import torch
-import core.inference.inference as inf
+from core.inference.inference import Canvas
 
 
 def parse_args() -> argparse:
-    """
-        Parses the inference arguments.
-    """
+    """Parse the inference arguments."""
+    # TODO: too many arguments here, move most to a settings/config file.
     parser = argparse.ArgumentParser(description='Inference Arguments Parser')
 
     parser.add_argument('--data', type=str, required=True,
                         help='Path to input images')
 
-    parser.add_argument('--seed', type=str, default='',
+    parser.add_argument('--seed', type=str, required=True,
                         help='seeds coordinates')
 
     parser.add_argument('--model', type=str, required=True,
@@ -74,13 +74,8 @@ def parse_args() -> argparse:
     return parser.parse_args()
 
 
-def canvas_init(args: argparse, process_id: int) -> Tuple[inf.Canvas, Dict]:
-    """
-    Initialize the FFN model and the Canvas.
-    """
-    images = tools.load_raw_image(args.data)
-    re_seged_count_mask = tools.resume_re_segd_count_mask(args.data_save,
-                                                          images.shape[:-1])
+def load_model(args: argparse) -> FFN:
+    """Load saved FFN model."""
     model = FFN(in_channels=4, out_channels=1, input_size=args.input_size,
                 delta=args.delta, depth=args.depth).cuda()
 
@@ -88,37 +83,43 @@ def canvas_init(args: argparse, process_id: int) -> Tuple[inf.Canvas, Dict]:
     model.load_state_dict(torch.load(args.model))
     model.eval()
 
-    canvas = inf.Canvas(model, images, args.input_size, args.delta,
-                        args.seg_thr, args.mov_thr, args.act_thr, args.flex,
-                        args.re_seg_thr, args.vox_thr, args.data_save,
-                        re_seged_count_mask, args.save_chunk,
-                        args.resume_seed, args.manual_seed, process_id)
-
-    seed_dict = {}
-    if os.path.exists(args.seed):
-        seed_dict = tools.seeds_to_dict(args.seed)
-
-    return canvas, seed_dict
+    return model
 
 
-def inf_run(args: argparse, canvas: inf.Canvas, inf_seed: Dict,
-            process_id: int, process_num: int) -> None:
-    """
-    Run the inference thread for all seeds.
-    """
+def canvas_init(args: argparse, process_id: int) -> Canvas:
+    """Initialize the Canvas."""
+    images = tools.load_raw_image(args.data)
+    re_seged_count_mask = tools.resume_re_segd_count_mask(args.data_save,
+                                                          images.shape[:-1])
+
+    model = load_model(args)
+
+    canvas = Canvas(model, images, args.input_size, args.delta,
+                    args.seg_thr, args.mov_thr, args.act_thr, args.flex,
+                    args.re_seg_thr, args.vox_thr, args.data_save,
+                    re_seged_count_mask, args.save_chunk,
+                    args.resume_seed, args.manual_seed, process_id)
+
+    return canvas
+
+
+def run(args: argparse, canvas: Canvas, seeds: Dict, process_id: int,
+        process_num: int) -> None:
+    """Run the inference thread for all seeds."""
     resume = tools.resume_dict_load(args.data_save, 'resume_seed',
                                     args.resume_seed)
-    for seed_id in inf_seed.keys():
+
+    for seed_id, coord in seeds.items():
         if not seed_id % process_num == process_id:
             continue
         if seed_id < args.resume_seed:
             continue
-        coord = inf_seed[seed_id]
         coord = tuple(coord)
 
         # Seed location limiter
         if coord[1] > 130:
             continue
+
         if canvas.segment_at(coord, seed_id, args.tag):
             if seed_id % 100 == 0:
                 resume['resume_seed'] = seed_id
@@ -127,24 +128,17 @@ def inf_run(args: argparse, canvas: inf.Canvas, inf_seed: Dict,
 
 
 def main(args: argparse) -> None:
-    """
-    Multiprocess the inference run.
-    """
+    """Multiprocess the inference run."""
     threads = args.threads
 
     for thread in range(threads):
-        canvas_inf, inf_seed_dict = canvas_init(args, thread)
-
-        # Manual seed run
-        inf_seed_dict = {1: [80, 65, 63], 2: [80, 51, 46], 3: [75, 43, 23],
-                         4: [77, 46, 33], 5: [76, 77, 77],
-                         6: [65, 83, 88], 7: [53, 86, 100],
-                         8: [26, 87, 105], 9: [40, 87, 100], 10: [78, 49, 39]}
+        canvas = canvas_init(args, thread)
+        seeds = tools.load_seeds_from_file(args.seed, args.manual_seed)
 
         # Main process
-        canvas_thread = threading.Thread(target=inf_run,
-                                         args=(args, canvas_inf, inf_seed_dict,
-                                               thread, threads))
+        canvas_thread = threading.Thread(target=run,
+                                         args=(args, canvas, seeds, thread,
+                                               threads))
         canvas_thread.start()
 
 
